801a4f20a484903b47627faf95bffe4be7ee098e
Sharjeel Ahmad <66735435+SharjeelJathol@users.noreply.github.com>
2024-08-13 14:43:20 +0500

    File Structure
    Separating the different actors of the system and creating separate mini-app routes. Eventually mounting them to the main application.

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-const fs=require("fs")
 const express=require("express")
 const morgan=require("morgan")
 
-const app=express()
+const tourRouter=require('./routes/tourRoutes.js')
+const userRouter=require('./routes/userRoutes.js')
 
-// express Routers
-const tourRouter=express.Router()
-const userRouter=express.Router()
+const app=express()
 
 // Middlewares
 app.use(morgan('dev'))
 app.use(express.json())
-
-
-const tours=JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-)
 
 app.use((req, res, next)=>{
     console.log("Middleware")
@@ -35,129 +28,6 @@ app.use((req, res, next)=>{
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-// Route handlers
-const getAllTours=(req, res)=>{
-    res.status(200).json({
-        status:"success",
-        results:tours.length,
-        data:{
-            tours
-        }
-    })
-}
-
-const getTour=(req, res)=>{
-    const id=req.params.id*1
-    // if(id >= tours.length){
-    //     return res.status(404).send({
-    //         status:"fail",
-    //         message:"Invalid ID"
-    //     })
-    // }
-    
-    const tour=tours.find(el=>el.id===id)
-    if(!tour){
-        return res.status(404).send({
-            status:"fail",
-            message:"Invalid ID"
-        })
-    }
-
-    res.status(200).json({
-        status:"success",
-        data:{
-            tour
-        }
-    })
-}
-
-const createTour=(req, res)=>{
-    const newTourId=tours[tours.length-1].id+1
-    const newTourData=Object.assign({id:newTourId}, req.body)
-    tours.push(newTourData)
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
-        res.status(201).json({
-            status:"success",
-            data:{
-                tour:newTourData
-            }
-        })
-    })
-}
-
-const updateTour=(req, res)=>{
-    const id=req.params.id*1
-    
-    const tour=tours.find(el=>el.id===id)
-    if(!tour){
-        return res.status(404).send({
-            status:"fail",
-            message:"Invalid ID"
-        })
-    }
-
-    res.status(200).json({
-        status:"success",
-        data:{
-            tour:"<Updated tour here...>"
-        }
-    })
-}
-
-const deleteTour=(req, res)=>{
-    const id=req.params.id*1
-    
-    const tour=tours.find(el=>el.id===id)
-    if(!tour){
-        return res.status(404).send({
-            status:"fail",
-            message:"Invalid ID"
-        })
-    }
-
-    res.status(200).json({
-        status:"success",
-        data:null
-    })
-}
-
-const getAllUsers=(req, res)=>{
-    res.status(500).json({
-        status:"error",
-        message:"This route is not yet defined"
-    })
-}
-
-const createUser=(req, res)=>{
-    res.status(500).json({
-        status:"error",
-        message:"This route is not yet defined"
-    })
-}
-
-const getUser=(req, res)=>{
-    res.status(500).json({
-        status:"error",
-        message:"This route is not yet defined"
-    })
-}
-
-const updateUser=(req, res)=>{
-    res.status(500).json({
-        status:"error",
-        message:"This route is not yet defined"
-    })
-}
-
-const deleteUser=(req, res)=>{
-    res.status(500).json({
-        status:"error",
-        message:"This route is not yet defined"
-    })
-    
-}
-
 // Routes
 // app.get('/api/v1/tours', getAllTours)
 // app.get('/api/v1/tours/:id', getTour)
@@ -165,27 +35,6 @@ const deleteUser=(req, res)=>{
 // app.patch('/api/v1/tours/:id', updateTour)
 // app.delete('/api/v1/tours/:id', deleteTour)
 
-tourRouter
-    .route("/")
-    .get(getAllTours)
-    .post(createTour)
-
-tourRouter
-    .route('/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour)
-
-userRouter 
-    .route('/')
-    .get(getAllUsers)
-    .post(createUser)
-
-userRouter
-    .route('/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser)
 
 app.listen(8000, ()=>{
     console.log("Server is listening at port 8000")
